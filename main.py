587af528cacd87a165dc46057442f99326745594
lgonz041 <lgonz041@ucr.edu>
2018-06-04 11:36:24 -0700

    added report

--- main.py
+++ main.py
@@ -49,13 +49,6 @@ def k_means_cs171(x_input,           #this is a datapoint x feature) matrix. Eac
                     temp_count[y] += 1
         for n in range(k):
             init_centroids[n] = np.true_divide(temp_centroid[n],temp_count[n])
-        # print("New centroids ")
-        # print(init_centroids)
-        # print("new_Assignments")
-        # print(new_assignment)
-        # print("old_assignment")
-        # print(old_assignment)
-        # raw_input()
 
     return new_assignment, init_centroids    #left is vector with data point and cluster Assignment. right final centroids
 #****************************************
@@ -65,37 +58,45 @@ def sum_of_square_of_errors(cluster_centroids, cluster_assignments, data ):
     total_error = 0.0
     for i in range(np.size(cluster_assignments)):
         for j in range(np.size(cluster_centroids,0)):
-            if(cluster_assignments[i] == j):
-                total_error += distance(data[i],cluster_centroids[j],2)
+            if(cluster_assignments[i] == j):        #only add distance from assign centroid to point
+                total_error += distance(data[i],cluster_centroids[j],2)**2  #sum up the distances from all points squared
     return total_error
 
 
-def Knee_Plot(iris_data):
+def Knee_Plot(iris_data,pp):
     cluster_assignments = [None]*10
     cluster_centroids = [None]*10
     errors = [None]*10
     #need to iterate through k from 1-10
     for k in range(1,11):
         print("Using K: "+str(k))
-        centroids = np.array([])
-        #generate a non-repetitive random number for the centrods
-        rand_cent = np.arange(150)
-        np.random.shuffle(rand_cent)
-        for i in range(1,k+1):
-            centroids = np.append(centroids,iris_data[rand_cent[i]],axis = 0)
-        centroids = centroids.reshape(k,4)
+
+        if(pp): #using kmean++ centroid initizliation
+            centroids = kmeanspp(iris_data,k)
+        else:   #using random centroids
+            centroids = np.array([])
+            #generate a non-repetitive random number for the centroids
+            rand_cent = np.arange(150)
+            np.random.shuffle(rand_cent)
+            for i in range(1,k+1):
+                centroids = np.append(centroids,iris_data[rand_cent[i]],axis = 0)
+            #centroid in proper position
+            centroids = centroids.reshape(k,4)
         print("Initial centroid")
         print(centroids)
+        #
         cluster_assignments[k-1], cluster_centroids[k-1] = k_means_cs171(x_input=iris_data,k=k,init_centroids=centroids)
         errors[k-1] = sum_of_square_of_errors(cluster_centroids[k-1],cluster_assignments[k-1],iris_data)
+        #print("For k: " +str(k)+" error "+str(errors[k-1]))
+
     return errors
 
-def Sensitivity_analysis(iris_data, max_inter):
+def Sensitivity_analysis(iris_data, max_inter, pp):
     errors =[None]*max_inter
     for n in range(max_inter):
-        errors[n] = Knee_Plot(iris_data)
+        errors[n] = Knee_Plot(iris_data,pp)     #get errors for the Knee_Plot y axis
 
-    Knee_plot = plt.figure(1)
+    Knee_plot = plt.figure(1)                   #plot graph
     x = np.arange(1, 11)
     if(max_inter == 1):
         y = errors[0]
@@ -112,6 +113,100 @@ def Sensitivity_analysis(iris_data, max_inter):
     plt.show()
     raw_input()
 
+#****************************************
+#K-means++ initizliation[35%]-Extra credit
+#****************************************
+def kmeanspp(x_input, k):
+    centroid = np.array([])
+    #choose first random centroid
+    x = np.random.randint(0,150)
+    centroid = np.append(centroid,x_input[x],axis=0).reshape(1,4)
+    dist = np.zeros((150,2))
+    #Get distance to all other points to the closes centroid
+    for i in range(1,k):
+        for j in range(np.size(x_input,0)):
+            temp_dist = np.array([])
+            for n in range(np.size(centroid,0)):
+                temp_dist = np.append(temp_dist,distance(x_input[j],centroid[n],2)**2)
+            dist[j] = [np.amin(temp_dist),j]
+
+        all_dist = np.sum(dist[:,0])    #sum of all the distance
+        dist = dist[dist[:,0].argsort()] #sort by distance and perserve index
+        P = np.zeros((150,2))   #set up all the distances in a range from [0-1]
+        temp_sum = 0.0
+        for i in range(np.size(P,0)):
+            temp_sum += dist[i][0]
+            P[i] = [temp_sum / all_dist,dist[i][1]]
+        #random number [0-1] that will be used to determine the index on the next centroid
+        r = np.random.rand()
+        for i in range(1, np.size(P,0)):
+            if P[i-1][0] < r and r <= P[i][0]:
+                centroid = np.append(centroid,x_input[int(P[i][1])].reshape(1,4),axis=0)
+                break
+    return centroid
+
+#****************************************
+#Top data points [15%]-Extra credit
+#****************************************
+def Top_points(iris_data,iris_name):
+    #Identify the top 3 data points and match the labels
+    cluster_assignments = [None]*10
+    cluster_centroids = [None]*10
+    errors = [None]*10
+    #need to iterate through k = 3
+    k = 3
+
+    centroids = np.array([])
+    #generate a non-repetitive random number for the centroids
+    rand_cent = np.arange(150)
+    np.random.shuffle(rand_cent)
+    for i in range(1,k+1):
+        centroids = np.append(centroids,iris_data[rand_cent[i]],axis = 0)
+    #centroid in proper position
+    centroids = centroids.reshape(k,4)
+    print("Initial centroid")
+    print(centroids)
+    cluster_assignments, cluster_centroids = k_means_cs171(x_input=iris_data,k=k,init_centroids=centroids)
+    dist1 = np.zeros((150,2))
+    dist2 = np.zeros((150,2))
+    dist3 = np.zeros((150,2))
+    for i in range(np.size(iris_data,0)):
+        dist1[i][0] = distance(iris_data[i],cluster_centroids[0],2)
+        dist1[i][1] = i
+        dist2[i][0] = distance(iris_data[i],cluster_centroids[1],2)
+        dist2[i][1] = i
+        dist3[i][0] = distance(iris_data[i],cluster_centroids[2],2)
+        dist3[i][1] = i
+
+    #sorting will give us the top three data points
+    dist1 = dist1[dist1[:,0].argsort()]
+    dist2 = dist2[dist2[:,0].argsort()]
+    dist3 = dist3[dist3[:,0].argsort()]
+    #Print top 3 class labels for the 3 clusters
+
+    print("Cluster: 1")
+    print(iris_name[int(dist1[0][1])])
+    print(iris_name[int(dist1[1][1])])
+    print(iris_name[int(dist1[2][1])])
+
+    print("Cluster: 2")
+    print(iris_name[int(dist2[0][1])])
+    print(iris_name[int(dist2[1][1])])
+    print(iris_name[int(dist2[2][1])])
+
+    print("Cluster: 3")
+    print(iris_name[int(dist3[0][1])])
+    print(iris_name[int(dist3[1][1])])
+    print(iris_name[int(dist3[2][1])])
+
 if __name__ == "__main__":
     iris_data, iris_name = Get_Data()
-    Sensitivity_analysis(iris_data,100)
+    #**************How to run*****************************
+    #First parameter data to cluster
+    #Second parameter number of iterations over data
+    #Third parameter bool pp use K-means++ if true else random initizliation
+    Sensitivity_analysis(iris_data,1,pp = False)
+
+
+
+    Top_points(iris_data,iris_name)
